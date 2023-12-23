77f752fccf94ad1c32dd24d32bbf3334aaa375f2
sushmit <sushmitsarmah@gmail.com>
2023-12-23 18:15:03 +0530

    created struct and updated

--- src/cli_parser/display_table.rs
+++ src/cli_parser/display_table.rs
@@ -1,25 +1,39 @@
 use prettytable::{Table, row};
 
-use crate::etherscan::account::AccTxn;
+// use crate::etherscan::account::AccTxn;
 
-pub fn display_table(mut data: Vec<AccTxn>) {
+pub struct TableRow {
+    block_number: String,
+    time_stamp: String,
+    from: String,
+    to: String,
+    value: String,
+}
+
+impl TableRow {
+    pub fn new(block_number: String, time_stamp: String, from: String, to: String, value: String) -> Self {
+        TableRow { block_number, time_stamp, from, to, value }
+    }
+
+    pub fn add_table_row(&self, table: &mut Table) {
+        table.add_row(row![
+            &self.block_number,
+            &self.time_stamp,
+            &self.from,
+            &self.to,
+            &self.value,
+        ]);
+    }
+}
+
+pub fn display_table_new(data: Vec<TableRow>) {
     let mut table = Table::new();
 
     table.add_row(row!["Block Number", "TimeStamp", "From", "To", "Value"]);
 
-    loop {
-        match data.pop() {
-            Some(txn) => {
-                table.add_row(row![
-                    txn.block_number,
-                    txn.time_stamp,
-                    txn.from,
-                    txn.to,
-                    txn.value,
-                ]);
-            },
-            None => break,
-        }
+    for i in 0..data.len() {
+        data[i].add_table_row(&mut table);
     }
+
     table.printstd();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,43 @@ mod etherscan;
 
 // use module function to get arguments from cli
 use cli_parser::clap_opts::CargoCli;
+use cli_parser::display_table::{display_table_new, TableRow};
+use etherscan::account::AccTxn;
+
+pub async fn fetch_eth_txns_data(
+    address: String,
+    etherscan_key:String
+) -> Result<Vec<AccTxn>, Box<dyn std::error::Error>> {
+    let txns = etherscan::account::get_account_txns(
+        address.as_str(),
+        etherscan_key
+    ).await?;
+
+    Ok(txns)
+}
 
 pub async fn init_fn(etherscan_key: String) -> Result<(), Box<dyn std::error::Error>> {
-
     let CargoCli::Account(args) = CargoCli::parse();
 
     if let Some(address) = args.account {
         if let Some(action) = args.action {
             if action == "txns" {
-                let txns = etherscan::account::get_account_txns(
-                    address.as_str(),
-                    etherscan_key
-                ).await?;
-                // println!("{:#?}", txns);
-                cli_parser::display_table::display_table(txns);
+                let txns = fetch_eth_txns_data(address, etherscan_key).await?;
+
+                let rows: Vec<TableRow> = txns
+                    .iter()
+                    .map(|txn| {
+                        TableRow::new(
+                            txn.block_number.to_string(),
+                            txn.time_stamp.to_string(),
+                            txn.from.to_string(),
+                            txn.to.to_string(),
+                            txn.value.to_string(),
+                        )
+                    })
+                    .collect();
+
+                display_table_new(rows);
             } else {
                 let balance =
                     etherscan::account::get_account_balance(address.as_str(), etherscan_key)
@@ -40,4 +63,3 @@ pub async fn init_fn(etherscan_key: String) -> Result<(), Box<dyn std::error::Er
 
     Ok(())
 }
-
